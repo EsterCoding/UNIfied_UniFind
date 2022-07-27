e7d8d625e71714b50e59c908e8e76cf15528cfca
ivaaMeco <ivameco06@gmail.com>
2022-07-27 22:52:47 +0100

    BUTONA

--- src/AbroadList.js
+++ src/AbroadList.js
@@ -2,27 +2,25 @@ import { Link, Routes, Route } from 'react-router-dom'
 
 function AbroadList() {
     return (
-        <div>
-            <h1> Universities Abroad</h1>
-
-            <nav>
-                <li>
+        <div className='div' align="center">
+            <h1> Universities Abroad </h1>
+            <div class="alb-university-buttons">
+                <button class="unis">
                     <Link to="/universities/Abroad/AbroadCS"> Computer Science </Link>
-                </li>
-                <li>
+                </button>
+                <button class="unis">
                     <Link to="/universities/Abroad/AbroadEconomy">  Economy </Link>
-                </li>
-                <li>
+                </button>
+                <button class="unis">
                     <Link to="/universities/Abroad/AbroadArch"> Architecture </Link>
-                </li>
-                <li>
+                </button>
+                <button class="unis">
                     <Link to="/universities/Abroad/AbroadLaw">  Law </Link>
-                </li>
-                <li>
+                </button>
+                <button class="unis">
                     <Link to="/universities/Abroad/AbroadMedicine">  Medicine </Link>
-                </li>
-            </nav>
-
+                </button>
+            </div>
         </div>
     )
 }
